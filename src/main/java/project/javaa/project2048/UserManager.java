a11c0ddbc3ecb9cd5993e4cf69a6561b569ebdb2
simontombu <63491676+simontombu@users.noreply.github.com>
2024-06-05 09:02:13 +0800

    Fixed errors occured during reading fils

--- src/main/java/project/javaa/project2048/UserManager.java
+++ src/main/java/project/javaa/project2048/UserManager.java
@@ -2,37 +2,40 @@ package project.javaa.project2048;
 
 import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
 public class UserManager {
     private Map<String, String> userData = new HashMap<>();
-    private String path;
+    private Path path;
 
     public UserManager() {
         loadUserDataFromFile();
     }
 
     private void loadUserDataFromFile() {
-        if (!Files.exists(Paths.get("").getParent().getParent().resolve("resources/project.javaa.project2048/data.dat"))) {
-            try {
-                Files.createFile(Paths.get("").getParent().getParent().resolve("resources/project.javaa.project2048/data.dat"));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        path = Paths.get("").getParent().getParent().resolve("resources/project.javaa.project2048/data.dat").toString();
+        path = Paths.get("").resolve("src/main/resources/project/javaa/project2048/data.dat");
 
         try {
-            FileInputStream fis = new FileInputStream(path);
+            if (!Files.exists(path)) {
+                Files.createFile(path);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            FileInputStream fis = new FileInputStream(path.toAbsolutePath().toString());
             ObjectInputStream ois = new ObjectInputStream(fis);
             userData = (Map<String, String>) ois.readObject();
             ois.close();
             fis.close();
-
-            System.out.println(userData);
+        } catch (EOFException e) {
+            register("Administrator", "admin");
+            saveUserDataToFile();
+            loadUserDataFromFile();
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
@@ -40,7 +43,7 @@ public class UserManager {
 
     private void saveUserDataToFile() {
         try {
-            FileOutputStream fos = new FileOutputStream(path);
+            FileOutputStream fos = new FileOutputStream(path.toAbsolutePath().toString());
             ObjectOutputStream oos = new ObjectOutputStream(fos);
             oos.writeObject(userData);
             oos.close();
